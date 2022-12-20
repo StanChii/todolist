4cd999919dd36dd1cc7f73b3f61a5be50622d391
stanley <stanleykeyi19@gmail.com>
2022-12-20 09:58:32 +0100

    first commit

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -131,7 +131,6 @@ STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 CORS_ALLOWED_ORIGINS=[
